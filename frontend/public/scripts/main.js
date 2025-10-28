9be84d3f896c2a8bd7f95a36c3f1b7fa39e22525
alealonso17 <2alejandroalonso70@gmail.com>
2025-10-28 02:42:48 +0000

    added server and register form

---
+++ frontend/public/scripts/main.js
@@ -0,0 +1,25 @@
+const registerForm = document.getElementById("registerForm"); //We get the form 
+
+
+registerForm.addEventListener("submit", async (e) => {
+    const user_handle = document.getElementById("user_handle").value.trim();  
+    const password = document.getElementById("password").value.trim();  
+    const email = document.getElementById("email").value.trim();  
+    const first_name = document.getElementById("first_name").value.trim();  
+    const last_name = document.getElementById("last_name").value.trim();  
+    const university = document.getElementById("university").value.trim(); 
+    
+    try{
+        const response = await fetch("http://localhost:3000/register", {
+            method : 'POST',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({user_handle, password, email, first_name, last_name, university})
+        })
+    }catch(err){
+        console.log("Error ocurred while fetch 🛜❌"); 
+        console.error(err); 
+    }
+
+})

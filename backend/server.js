10e717c783f042b5054b14b2c11e5eccd848f4c4
alealonso17 <2alejandroalonso70@gmail.com>
2025-10-28 02:42:48 +0000

    added server and register form

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,48 @@
 import express from "express"; 
 import cors from "cors"; 
 import bcrypt, { hash } from "bcrypt"; 
+import mysql2 from "mysql2";
+
 
 const app = express(); 
-app.use(cors()); 
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json()); 
 
+const conection = mysql2.createConnection({
+    host : "mysql://root:IVnnRwnJNIRgvwSCNZyzXArWqJyzBaJB@shuttle.proxy.rlwy.net:50152/railway",
+    user : "root",
+    database : "railway",
+    password : "IVnnRwnJNIRgvwSCNZyzXArWqJyzBaJB",
+    port : "3306"
+
+})
 
 
+app.post("/register", async (req, res) => {
+    const {user_handle, password, email, first_name, last_name, university} = req.body;
+    const hashedPass = await bcrypt.hash(password, 10); 
+
+    try{
+
+        await conection.query(
+            'INSERT INTO users (user_handle, email_address, password_hash, first_name, last_name, university) VALUES (?, ?, ?, ?, ?, ?)',
+            [user_handle, email, hashedPass, first_name, last_name, university]
+        ); 
+        console.log("User registered Correctly ✅");
+
+    }catch(err){
+
+        console.log("Error registering the user ❌🛜"); 
+        console.error(err); 
+    }
+
+
+
+})
 
 
 app.listen(3000, () => {

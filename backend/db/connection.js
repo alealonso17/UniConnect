c82cc9b1e6d866438fde70700ad7f851619bd253
alealonso17 <2alejandroalonso70@gmail.com>
2025-10-28 13:27:48 +0000

    added authentification methods

---
+++ backend/db/connection.js
@@ -0,0 +1,18 @@
+import mysql2 from 'mysql2'; 
+
+export let conection; 
+
+try{ //try to connect with await , take your time , dont want it to execute after the query 
+
+    conection = await mysql2.createConnection({
+    host : "mysql.railway.internal",
+    user : "root",
+    database : "railway",
+    password : "IVnnRwnJNIRgvwSCNZyzXArWqJyzBaJB",
+    port : "3306"
+    }); 
+
+    console.log("mySQL conection stablishes ✅🛜");
+}catch(err){
+    console.log("❌ Failed to connect to MySQL:", err);
+}
